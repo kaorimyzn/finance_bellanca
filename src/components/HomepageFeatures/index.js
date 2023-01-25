1e5ad4676b2923dda86093ea8243d4f0563ad89d
kaorimyzn <98532529+kaorimyzn@users.noreply.github.com>
2023-01-25 20:13:33 +0700

    ubah svg to src

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Mudah',
-    Svg: require('https://img.freepik.com/free-vector/investor-with-laptop-monitoring-growth-dividends-trader-sitting-stack-money-investing-capital-analyzing-profit-graphs-vector-illustration-finance-stock-trading-investment_74855-8432.jpg?w=826&t=st=1674650186~exp=1674650786~hmac=b1f5a9792ec14da68e50514245038af8aedcbc161291a8569dcb76cfa655eb9f').default,
+    src: ('https://img.freepik.com/free-vector/investor-with-laptop-monitoring-growth-dividends-trader-sitting-stack-money-investing-capital-analyzing-profit-graphs-vector-illustration-finance-stock-trading-investment_74855-8432.jpg?w=826&t=st=1674650186~exp=1674650786~hmac=b1f5a9792ec14da68e50514245038af8aedcbc161291a8569dcb76cfa655eb9f').default,
     description: (
       <>
         Mudahkan pengelolaan keuangan usaha Anda dengan aplikasi pembukuan kami
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Handal',   
-    Svg: require('https://img.freepik.com/free-vector/happy-women-sitting-talking-each-other-dialog-psychologist-tablet-flat-illustration_74855-14078.jpg?w=826&t=st=1674650212~exp=1674650812~hmac=580e09feeff3ab278c0a57bbd3d1c7395495958894d7814d2d09dafa2b0f63f9').default,
+    src: ('https://img.freepik.com/free-vector/happy-women-sitting-talking-each-other-dialog-psychologist-tablet-flat-illustration_74855-14078.jpg?w=826&t=st=1674650212~exp=1674650812~hmac=580e09feeff3ab278c0a57bbd3d1c7395495958894d7814d2d09dafa2b0f63f9').default,
     description: (
       <>
        Pembukuan keuangan yang mudah dan handal untuk usaha kecil Anda
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Canggih',
-    Svg: require('https://img.freepik.com/free-vector/money-income-attraction_74855-6573.jpg?w=740&t=st=1674650148~exp=1674650748~hmac=2f2c79512b51bcba90abfdb455285c3ddd308b120b97efb1d6c31e1bfc560d53').default,
+    src: ('https://img.freepik.com/free-vector/money-income-attraction_74855-6573.jpg?w=740&t=st=1674650148~exp=1674650748~hmac=2f2c79512b51bcba90abfdb455285c3ddd308b120b97efb1d6c31e1bfc560d53').default,
     description: (
       <>
         Efisiensi keuangan usaha Anda dengan aplikasi pembukuan yang canggih
